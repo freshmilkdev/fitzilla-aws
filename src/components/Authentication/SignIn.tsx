1e255977fddd9e905b8ed631e9252c4372ca1583
Max <max.ruchkin@gmail.com>
2020-11-17 16:33:21 +0200

    basic auth screen

---
+++ src/components/Authentication/SignIn.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {AuthForm} from "./AuthForm";
+import {FormTypeEnum} from "./Authentication";
+
+type SignUpProps = {
+    onChange: Function,
+    onChangeFormType: Function,
+    username: string,
+    email: string,
+    password: string,
+    passwordConfirm: string
+}
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+export const SignUp: React.FC<SignUpProps> =
+    ({onChange, onChangeFormType, username, email, password, passwordConfirm}) => {
+        const classes = useStyles();
+        return (
+            <AuthForm header={'Sign In'}>
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                />
+                <FormControlLabel
+                    control={<Checkbox value="remember" color="primary"/>}
+                    label="Remember me"
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                >
+                    Sign In
+                </Button>
+                <Grid container>
+                    <Grid item xs>
+                        <Link href="#" variant="body2">
+                            Forgot password?
+                        </Link>
+                    </Grid>
+                    <Grid item>
+                        <Link
+                            component="button"
+                            variant="body2"
+                            type="button"
+                            onClick={() => onChangeFormType(FormTypeEnum.SignUp)}
+                        >
+                            Don't have an account? Sign Up
+                        </Link>
+                    </Grid>
+                </Grid>
+            </AuthForm>
+        )
+    }
